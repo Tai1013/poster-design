8a9d01fd95ddf47538464dc19a5303ae5b8bffdc
Tai1013 <frank22199215@gmail.com>
2024-02-16 02:47:28 +0800

    chore: create eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      vue()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, 'src/')
+      }
+    }
+  }
 })
