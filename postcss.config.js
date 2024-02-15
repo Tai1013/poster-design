75eaa614f74bc183cdfdb2026268cca6f813eea5
Tai1013 <frank22199215@gmail.com>
2024-02-16 02:23:04 +0800

    chore: create sass postcss autoprefixer

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+export default {
+  plugins: {
+    autoprefixer: [
+      '> 1%',
+      'last 5 versions',
+      'Chrome >= 60',
+      'Firefox >= 60',
+      'not Edge < 79',
+      'iOS >= 10',
+      'Safari >= 10',
+      'Android >= 6',
+      'not Explorer <= 11'
+    ]
+  }
+}
