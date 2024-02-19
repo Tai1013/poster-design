b58ace8e5df9dca5badd74537cded8b8b4d2bd00
Tai1013 <frank22199215@gmail.com>
2024-02-19 16:12:20 +0800

    feat: home header layout

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,19 +1,88 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { ElTabs, ElTabPane } from 'element-plus'
+import { PosterButton } from '@/components/basic'
+import { storeToRefs, useHistoryStore } from '@/stores'
 import SidebarComp from './Sidebar.vue'
 import ViewprotComp from './Viewprot.vue'
 import AttributesComp from './Attributes.vue'
 import LayerComp from './Layer.vue'
 
+type HeaderButtonType = {
+  icon: GlobalApp.IconType
+  label?: string
+  disabled?: boolean
+  onClick: () => void
+}
+
 defineOptions({ name: "PosterHome" })
 
+const { undoable, redoable } = storeToRefs(useHistoryStore())
+const { handleHistory } = useHistoryStore()
+
 const activeTab = ref("attributes")
+
+const headerLeftButtons = computed((): HeaderButtonType[] => [
+  {
+    icon: 'Bars3Icon',
+    onClick: () => {}
+  },
+  {
+    icon: 'ArrowUturnLeftIcon',
+    disabled: !undoable.value,
+    onClick: () => handleHistory('undo')
+  },
+  {
+    icon: 'ArrowUturnRightIcon',
+    disabled: !redoable.value,
+    onClick: () => handleHistory('redo')
+  }
+])
+
+const headerRightButtons = computed((): HeaderButtonType[] => [
+  {
+    label: '重置',
+    icon: 'ArrowPathIcon',
+    onClick: () => {}
+  },
+  {
+    label: '預覽',
+    icon: 'EyeIcon',
+    onClick: () => {}
+  },
+  {
+    label: '保存',
+    icon: 'DocumentPlusIcon',
+    onClick: () => {}
+  }
+])
 </script>
 
 <template lang="pug">
 .poster-layout
   header
+    div
+      PosterButton(
+        v-for="button in headerLeftButtons"
+        :key="button.icon"
+        type="primary"
+        size="small"
+        :icon="button.icon"
+        icon-size="16"
+        :disabled="button.disabled"
+        @click="button.onClick"
+      )
+    div
+      PosterButton(
+        v-for="button in headerRightButtons"
+        :key="button.icon"
+        type="primary"
+        size="small"
+        :icon="button.icon"
+        icon-size="16"
+        :disabled="button.disabled"
+        @click="button.onClick"
+      ) {{ button.label }}
   .poster-content
     SidebarComp(class="flex-shrink-0 shadow")
     ViewprotComp(class="flex-auto")
@@ -35,7 +104,15 @@ header, footer {
 }
 
 header {
-  @apply bg-primary-400 top-0;
+  @apply flex justify-between items-center bg-primary-400 top-0;
+
+  > div {
+    @apply flex items-center;
+  }
+
+  .el-button {
+    @apply m-0 h-10 text-black disabled:border-none disabled:bg-transparent disabled:opacity-60 disabled:hover:text-black;
+  }
 }
 
 footer {
