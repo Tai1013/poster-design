f700865cf557e68fc634607483dcacddd99ef539
Tai1013 <frank22199215@gmail.com>
2024-02-19 16:05:03 +0800

    chore: delete @element-plus/icons-vue

--- src/plugins/element-plus.ts
+++ src/plugins/element-plus.ts
@@ -1,7 +1,4 @@
 import 'element-plus'
-import 'element-plus/dist/index.css'
-import 'element-plus/theme-chalk/dark/css-vars.css'
-
 import { Plugin } from 'vue'
 import { ElLoading } from 'element-plus'
 
